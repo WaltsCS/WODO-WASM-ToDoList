41268470ddb6f65c8115efcf17525955bd3e4614
Walton Karl <100638896+WaltsCS@users.noreply.github.com>
2025-11-25 00:27:31 +0800

    Just-In-Case: for guaranteed update
    Just a small test

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wodo-cache-v3";
+const CACHE_NAME = "wodo-cache-v4";
 
 //auto-detects prefix (/WODO-WASM-ToDoList)
 const ROOT = self.location.pathname.replace(/\/sw\.js$/, "");
