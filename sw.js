8be806ee843cf34b44c27d804360c70e345e82f4
Walton Karl <100638896+WaltsCS@users.noreply.github.com>
2025-11-30 21:52:57 +0800

    Small modification to service worker to add auto-update and versioning

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
-const CACHE_NAME = "wodo-cache-v4";
+//auto-update & versioning
+//every new commit, bump up this number (notice blue side indicator)
+const VERSION = "v5";  // <--- bump to v8, v9, etc.
+
+const CACHE_NAME = `wodo-cache-${VERSION}`;
 
 //auto-detects prefix (/WODO-WASM-ToDoList)
 const ROOT = self.location.pathname.replace(/\/sw\.js$/, "");
@@ -15,39 +19,42 @@ const ASSETS = [
   `${ROOT}/sample.png`
 ];
 
+//cache everything
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("CACHE_NAME").then(cache => {
-            return cache.addAll(ASSETS);
-        })
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+
+  //immediately activate the new SW (auto-update)
+  self.skipWaiting();
 });
 
 //(activate) cleans old caches
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then(keys => 
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
       )
     )
   );
+
+  //immediately take control of clients
   self.clients.claim();
 });
 
 
-//(fetch) network w/ fallback for caching/offline page
+//(fetch) network(1st) w/ fallback for caching/offline page(2nd)
 self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request).then(cacheRes => {
-          return cacheRes || caches.match(OFFLINE_FALLBACK);
-        });
-      })
+      .then(response => response)
+      .catch(_ =>
+        caches.match(event.request).then(cacheRes =>
+          cacheRes || caches.match(OFFLINE_FALLBACK)
+        )
+      )
   );
 });
